0151176a54b473caab6b084515fe579e1cdbe4bf
Joseph LeGrand <joseph.legrand@entelect.co.za>
2024-09-24 23:17:49 +0200

    cater for prod URL

--- src/helpers/jwt.interceptor.ts
+++ src/helpers/jwt.interceptor.ts
@@ -3,18 +3,22 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable } from 'rxjs';
 import { AccountService } from 'src/app/shared/services/account.service';
 import { environment } from 'src/environments/environment';
-
-
+import { environment as prod_environment } from "src/environments/environment.prod";
+import { environment_variables } from "src/environments/env-variables";
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private accountService: AccountService) { }
+    apiUrl: string ="";
+
+    constructor(private accountService: AccountService) {
+        this.apiUrl = environment_variables.isLive? prod_environment.apiUrl : environment.apiUrl;
+     }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add auth header with jwt if user is logged in and request is to the api url
         const user = this.accountService.userValue;
         const isLoggedIn = user && user.token;
-        const isApiUrl = request.url.startsWith(environment.apiUrl);
+        const isApiUrl = request.url.startsWith(this.apiUrl);
         if (isLoggedIn && isApiUrl) {
             request = request.clone({
                 setHeaders: {
